c9c18492ffbcd1073fb528cf52595b18ae1b2b6c
Bob Foxx <bobfox141@gmail.com>
2025-08-04 17:05:23 -0500

    Launcher mods to add communication

--- controller.py
+++ controller.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
 # file: controller
-# this takes the place of a launch command system
-# 
+# this takes the place of a launch command system. It is effectively a client
+# in the client server system. The server listens for commands 
 
 import time
 import queue
-
+import socket
 
 class Controller:
-    
+    LAUNCHFREQ = 5
     QUIT = 0
     STANDBY = 1
     ARM = 2
@@ -21,30 +21,53 @@ class Controller:
     
     
     def __init__(self):
-        pass
+        self.done = False
+        self.host = '127.0.0.1'
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        
+        self.port = 54000
+        self.s.bind(('',self.port))
+        print("Bound socket on port: ", self.port)
+        
     
     def send(self, message):
+        # this will fail if the receiving connection isn't open
+        # with 'broken pipe'
+        self.s.send(message.to_bytes()) 
         
-        
+    def close(self):
+        self.s.close()
         
     
     def go(self, hz):
         waits = 1/hz
-        while not done:
-            command = input("Enter command [Q,S,A,L,O,T]: ")
+        while not self.done:
+            command = input("Enter a command [Q,S,A,L,O,T]: ")
             command = command.strip()
             command = command[0].upper()
             if command == "Q":
-                send(QUIT)
-            elif command = "S":
-                send(STANDBY)
-            elif command = "A":
-                send(ARM)
-            elif command = "L":
-                send(LAUNCH)
-            elif command = "O":
-                send(OFF)
-            elif command = "T":
-                send(TARGET)
-                send(coords)
+                self.send(self.QUIT)
+                self.done = True
+            elif command == "S":
+                self.send(self.STANDBY)
+            elif command == "A":
+                self.send(self.ARM)
+            elif command == "L":
+                self.send(self.LAUNCH)
+            elif command == "O":
+                self.send(self.OFF)
+            elif command == "T":
+                self.send(self.TARGET)
+                self.send(self.coords)
             time.sleep(waits)            
+    
+    def main(self, hz):
+        self.go(hz)
+
+
+
+if __name__ == "__main__":
+    print("Running controller.py in stand alone mode:")
+    c = Controller()
+    c.main(c.LAUNCHFREQ)
--- launcher.py
+++ launcher.py
@@ -6,6 +6,8 @@
 # and fires the charge that lights the fuel sources.
 # after that, it just returns to standby
 
+import socket
+
 class Launcher():
     OFF = 0
     STANDBY = 1
@@ -17,18 +19,28 @@ class Launcher():
     TAMIR = 2
     HARMS = 3
     JAVEL = 4
-       
+    
     def __init__(self, r, t):
-        fsm = OFF
-        launcher_type = fixed
-        pods_loaded = 1 
-        rounds = r
-        mtype = t
-        expended = 0
-        directional = true
-        target = [0,0,0]   # target is x,y,z downrange in meters
-        waypoint = [0,0,0] # a list of waypoints. first interation will not use this.
-        done = false
+        self.fsm = OFF
+        self.launcher_type = fixed
+        self.pods_loaded = 1 
+        self.rounds = r
+        self.mtype = t
+        self.expended = 0
+        self.directional = true
+        self.target = [0,0,0]   # target is x,y,z downrange in meters
+        self.waypoint = [0,0,0] # a list of waypoints. first interation will not use this.
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.port = 54000
+        self.s.bind(('',self.port))
+        self.s.listen(5)   # max 5 connections set up for the listen.
+        self.done = false
+        self.buffer = ""
+        
+    def recv(self):
+        self.buffer = self.s.recv(128)  # receive data up to 128 bytes and put it in the buffer.
+        return self.buffer
         
         
         
@@ -40,18 +52,28 @@ class Launcher():
     
     # this is the main loop for the launcher
     def go(self):
-        if self.fsm == OFF:
-            if DEBUG == true:
-                print("Launcher is off, waiting...")
-            sleep(1) # if the launcher is off, wait
-        else 
-            while (self.done == true):
+        while not self.done:
+            buf = self.recv()  # check the socket for a command
+            if length(buf) > 0:
+                command = buf.decode()
+            print("Current status: Launcher is ", end = "")
+            if self.fsm == self.OFF:
+                print("OFF\n")
+                if command == self.STANDBY:
+                    self.init()
+                    print("")
+                
+                
+
+
+
             
     
     # initialize currently turns the launcher from off to standby
     # checks the rounds available and then waits for a target
     def init(self):
         fsm = STANDBY
+        printf("Powering the launcher on.")
         
     def armlauncher(self):
         fsm = ARMED
